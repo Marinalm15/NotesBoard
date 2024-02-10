e7571acb9e5e6934ac5e36ead1ac8a07ea532b87
Marinalm15 <marinalm15@hotmail.com>
2024-02-10 13:08:03 -0300

    adiciona funcionalide de apagar notas e responsividade

--- src/app.tsx
+++ src/app.tsx
@@ -35,6 +35,16 @@ export function App() {
         localStorage.setItem('notes', JSON.stringify(notesArray))
     }
 
+    function onNoteDeleted(id: string) {
+        const notesArray = notes.filter(note => {
+            return note.id !== id
+        })
+
+        setNotes(notesArray)
+
+        localStorage.setItem('notes', JSON.stringify(notesArray))
+    }
+
     function handleSearch(event: ChangeEvent<HTMLInputElement>) {
         const query = event.target.value
 
@@ -46,7 +56,7 @@ export function App() {
         : notes
 
  return (
-    <div className='mx-auto max-w-6xl my-12 space-y-6'>
+    <div className='mx-auto max-w-6xl my-12 space-y-6 px-5'>
         <img src={logo} alt="logo" />
         <form className='w-full'>
             <input 
@@ -59,13 +69,13 @@ export function App() {
 
         <div className='h-px bg-slate-700'/>
 
-        <div className='grid grid-cols-3 gap-6 auto-rows-[250px]'>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 auto-rows-[250px]'>
         
         <NewNoteCard onNoteCreated={onNoteCreated}/>
         
         {filtredNotes.map(note => {
             return (
-                <NoteCard key={note.id} note={note}/>
+                <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/>
             )
         })}
         </div>
